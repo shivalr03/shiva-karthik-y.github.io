ca29a4920c9fd300c87919c1a76a44e3c74819c4
Shiva Karthik <55740157+shivalr03@users.noreply.github.com>
2025-10-24 02:16:02 +0530

    test

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   base: '/shiva-karthik-y.github.io/',
-   build: { outDir: 'docs' }
+   build: { outDir: 'docs' },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
