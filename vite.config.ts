49b899d0dc02d75a63c9b5a37ff1cd797942257a
shivalr03 <shivalr03@gmail.com>
2025-10-24 03:01:19 +0530

    fixed base

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ command, mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  base: '/shiva-karthik-y.github.io/',
+  base: command === 'build' ? '/shiva-karthik-y.github.io/' : '/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
